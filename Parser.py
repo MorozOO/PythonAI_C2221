26b4999e4154666a18dd530bf2a772b983fe8d55
Moroz <moroz.oo.itstep@gmail.com>
2025-04-12 17:27:07 +0300

    Lesson 12. Telegram bot

--- Parser.py
+++ Parser.py
@@ -1,5 +1,8 @@
+# https://web.telegram.org/a/#8180384450
 from bs4 import BeautifulSoup
 import requests
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
 url = "https://uaserials.pro/films/"
 
@@ -19,19 +22,43 @@ with open('link.txt', 'r') as file:
 
 # print(links_list)
 f = open('info.txt', 'w', encoding='utf-8')
-
+list_name = []
+list_desc = []
 for link in links_list:
     req = requests.get(link)
     soup1 = BeautifulSoup(req.text,features="html.parser" )
     soup_list_name_film = soup1.find_all('span', {"class":"oname_ua"})
     if len(soup_list_name_film)> 0:
         f.write(f'{soup_list_name_film[0].text}\n')
+        list_name.append(soup_list_name_film[0].text)
     soup_list_ul = soup1.find_all('ul',{"class":"short-list fx-1"})
     for item in soup_list_ul:
         f.write(f"{item.text}\n")
+        list_desc.append(item.text)
 
 f.close()
+command = """/help - список всіх команд бота
+/hello - привітання,
+/film - список найновіших фільмів"""
+async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text(f'Hello {update.effective_user.first_name}')
 
+async def film(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    for i in range(len(links_list)):
+        text = f"{list_name[i]}\n{list_desc[i]}\n{links_list[i]}"
+        await update.message.reply_text(text)
+
+async def menu(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text(command)
+
+
+app = ApplicationBuilder().token("8180384450:AAF9TZI-QqCG_ElLbN2bBhz3wI5KvkiAqCM").build()
+
+app.add_handler(CommandHandler("hello", hello))
+app.add_handler(CommandHandler("film", film))
+app.add_handler(CommandHandler("help", menu))
+
+app.run_polling()
 
 
 
