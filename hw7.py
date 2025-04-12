54286cb70491112d073523fb71ab104d86bf2df2
Moroz <moroz.oo.itstep@gmail.com>
2025-04-12 17:32:19 +0300

    hw7, hw10

---
+++ hw7.py
@@ -0,0 +1,16 @@
+# class iterGenerator:
+#     def __init__(self, n):
+#         self.n = n
+#
+#     def __iter__(self):
+#         for i in range(self.n):
+#             yield i
+#
+# objIterGenerator = iterGenerator(10)
+# print(objIterGenerator)
+# for val in objIterGenerator:
+#     print(val)
+
+x = 'hello'
+
+assert x == 'hello1', "Win!!!"
